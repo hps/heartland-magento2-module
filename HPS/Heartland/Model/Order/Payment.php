153fcea976f639bdb3fabe4791e8ce73a2a91c37
Charles Simmons <ttnnkkrr@users.noreply.github.com>
2016-12-15 23:02:34 +0000

    AutoCommit on edit [Thu, 15 Dec 2016 23:02:34 +0000] File saved: Payment.php___jb_bak___

--- HPS/Heartland/Model/Order/Payment.php
+++ HPS/Heartland/Model/Order/Payment.php
@@ -19,7 +19,7 @@ class Payment
     function canCapture()
     { //TODO: ensure that this is an authorization but the gateway will throw an error if this fails for now
         ;
-        return $this->getHPS()->get($this->getCcTransId())->transactionStatus === 'A';
+        return $this->getHPS()->get($this->getCcTransId())->transactionStatus === 'A'?true:false;
     }
 
     /**
