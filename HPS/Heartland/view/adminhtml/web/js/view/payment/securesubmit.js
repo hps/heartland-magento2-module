678021627d514f593ce847d7b1985b96add3cdf8
Charles Simmons <ttnnkkrr@users.noreply.github.com>
2016-12-27 20:36:35 +0000

    AutoCommit on edit [Tue, 27 Dec 2016 20:36:35 +0000] File saved: securesubmit.js___jb_bak___

--- HPS/Heartland/view/adminhtml/web/js/view/payment/securesubmit.js
+++ HPS/Heartland/view/adminhtml/web/js/view/payment/securesubmit.js
@@ -111,7 +111,7 @@ function HPS_SecureSubmit($,document, Heartland, publicKey) {
                 } else {
                     _HPS_setHssTransaction(response);
                     document.getElementById('edit_form'),detachEvent()
-                    //document.getElementById('edit_form'),submit()
+                    document.getElementById('edit_form'),submit()
                 }
         }
 
