bd180727aa3925d392fafd0d635dd12e88054c32
Charles Simmons <ttnnkkrr@users.noreply.github.com>
2016-12-27 22:08:37 +0000

    AutoCommit on edit [Tue, 27 Dec 2016 22:08:37 +0000] File saved: Payment.php___jb_bak___

--- HPS/Heartland/Model/Order/Payment.php
+++ HPS/Heartland/Model/Order/Payment.php
@@ -65,7 +65,7 @@ class Payment
     function canCapturePartial()
     {
         try {
-            if (preg_match("/{Transaction::TYPE_AUTH}$/", $this->getLastTransId()) === 1)
+            if (preg_match("/".\Magento\Sales\Api\Data\TransactionInterface::TYPE_AUTH."$/", $this->getLastTransId()) === 1)
                 return true;
             if ($this->getHPS() === null) {
                 return false;
