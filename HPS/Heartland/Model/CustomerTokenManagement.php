f11bceeec74c9615a6a4fa1b273b4c8f2dc3021e
Senthil Kumar Muthusamy <senthilkumar.m@HALNVDSCAPP51.hps.com>
2017-03-31 07:55:10 -0400

    code clean up

--- HPS/Heartland/Model/CustomerTokenManagement.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace HPS\Heartland\Model;
-
-use Magento\Customer\Model\Session;
-use Magento\Vault\Api\Data\PaymentTokenInterface;
-
-class CustomerTokenManagement {}
-// Magento\Vault\Block\Customer\CreditCards
